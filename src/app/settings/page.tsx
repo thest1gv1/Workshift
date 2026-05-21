3d1a3874d32c20e81c069f063a2768aed0d53faf
thest1gv1 <thest1gv1@gmail.com>
2026-05-21 14:27:36 +0500

    feat: add stats and settings pages

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,3 +1,27 @@
+import {Input} from "@/components/ui/input";
+
 export default function SettingsPage() {
-  return <h1>Настройки</h1>
+  return (
+    <div className="grid gap-5">
+      <h1>Настройки
+      </h1>
+      <div className="grid gap-2">
+        <label
+          htmlFor="name"
+          className="text-xs uppercase text-muted-foreground tracking-wider"
+        >Профиль
+        </label>
+        <Input
+          id="name"
+          placeholder="Ваше Фио"
+        />
+      </div>
+      <div className="grid gap-2">
+        <label htmlFor="plan" className="text-xs uppercase text-muted-foreground tracking-wider">
+          Дневной план
+        </label>
+        <Input id="plan" type="number" placeholder="8" />
+      </div>
+    </div>
+  )
 }
--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -1,3 +1,41 @@
+import StatBox from "@/components/shift/StatBox";
+
+
 export default function StatsPage() {
-  return <h1>Статистика</h1>
+  return (
+    <div className="grid gap-5">
+      <h1>Статистика</h1>
+      <div className="grid grid-cols-2 gap-2">
+        <StatBox
+          value={1}
+          label='Выдано'
+        />
+        <StatBox
+          value={0}
+          label='Клиентопоток'
+        />
+      </div>
+
+
+      <div className="grid gap-2">
+        <span className="text-xs uppercase text-muted-foreground tracking-wider">Услуги</span>
+        <div>
+          <ul className="grid gap-2">
+            <li className="flex justify-between items-center p-3 bg-card rounded-lg">
+              <span className=" text-muted-foreground">ДК</span>
+              <span className=" font-semibold text-primary">3</span>
+            </li>
+            <li className="flex justify-between items-center p-3 bg-card rounded-lg">
+              <span className=" text-muted-foreground">КЕШ</span>
+              <span className=" font-semibold text-primary">2</span>
+            </li>
+            <li className="flex justify-between items-center p-3 bg-card rounded-lg">
+              <span className=" text-muted-foreground">ДКО</span>
+              <span className=" font-semibold text-primary">4</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
 }
