7ea6bbe59dfa146234d1b3e768e144899213fc36
thest1gv1 <thest1gv1@gmail.com>
2026-06-09 15:57:09 +0500

    feat: add basePath/workshift

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+	basePath: '/workshift',
+}
 
-export default nextConfig;
+export default nextConfig
